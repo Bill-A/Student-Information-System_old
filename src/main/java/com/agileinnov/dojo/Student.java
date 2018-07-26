92c6fe41486dc05e1818f98b507e9df5ff7a704f
Bill Allen <BillAllenEmail@gmail.com>
2018-07-26 15:19:13 -0500

    Initial Passing Test

---
+++ src/main/java/com/agileinnov/dojo/Student.java
@@ -0,0 +1,13 @@
+package com.agileinnov.dojo;
+
+public class Student {
+    private String name;
+
+    public Student(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+}
